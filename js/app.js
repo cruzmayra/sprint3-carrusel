f143df6876e0791b723f758aa71ab631a779a44b
CruzMayra <velazquez.leonora@gmail.com>
2018-01-17 17:37:03 -0600

    Corriguiendo identación

--- js/app.js
+++ js/app.js
@@ -1,49 +1,50 @@
 var target = 0;
+
 $(document).ready(function(){
-  var $botones = $(".control");
+  var $buttons = $(".control");
   var $previous = $('.previous');
   var $next = $('.next');
 
-  $botones.click(cargarImagen);
+  $buttons.click(uploadImage);
   $previous.click(previousImage);
   $next.click(nextImage);
 
 })
 
-var cargarImagen = function () {
+var uploadImage = function() {
   var $target = $(this).data("target");
-  mostrarImagen($target);
+  showImage($target);
   changeColorButton($target);
   previousImage($target);
   nextImage($target);
 }
 
-var mostrarImagen = function (target) {
-  var $actualSlide = $("div.active");
+var showImage = function(target) {
+  var $currentSlide = $("div.active");
   var $newImage = $("div[data-slide=" + target + "]");
-  $actualSlide.removeClass("active");
+  $currentSlide.removeClass("active");
   $newImage.addClass("active");
 }
 
-var changeColorButton = function (target) {
-  var $actualButton = $("button.active");
-  var $newButton = $("button[data-target=" + target+ "]");
-  $actualButton.removeClass("active");
+var changeColorButton = function(target) {
+  var $currentButton = $("button.active");
+  var $newButton = $("button[data-target=" + target + "]");
+  $currentButton.removeClass("active");
   $newButton.addClass("active");
 }
 
-var previousImage = function (e) {
+var previousImage = function(e) {
   e.preventDefault();
   target = target - 1;
   target = (target < 0) ? 4 : target;
-  mostrarImagen(target);
+  showImage(target);
   changeColorButton(target);
 }
 
-var nextImage = function (e) {
+var nextImage = function(e) {
   e.preventDefault();
   target = target + 1;
   target = (target > 4) ? 0 : target;
-  mostrarImagen(target);
+  showImage(target);
   changeColorButton(target);
 }
